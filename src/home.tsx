bb51a7b9ceebf4d9fddf1d9d054dde77dcc04c55
Tortoise <jslegend119@gmail.com>
2022-06-10 00:58:22 -0400

    Fix quote

--- src/home.tsx
+++ src/home.tsx
@@ -95,10 +95,10 @@ const Home = () => {
     const quote = await sdk.getQuote(transferParams);
 
     console.log('Quote:', quote);
-    const transferRoute = quote.routes[0];
+    const transferRoute = quote.routes[0].route;
 
     console.log('Sending transfer...', transferRoute);
-    await sdk.transfer(transferRoute as any, transferParams);
+    await sdk.transfer(transferRoute, transferParams);
   };
 
   return (
